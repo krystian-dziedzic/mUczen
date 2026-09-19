c93a0127fd46887df16e7ec702b330feaebc683d
Krystian <krystian.dziedzic.dev@protonmail.com>
2026-09-19 13:27:21 -0700

    Animation tests

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -6,5 +6,14 @@
         {
             InitializeComponent();
         }
+
+        private async void Animate_Logo(object sender, EventArgs e)
+        {
+            while(true)
+            {
+                await Logo.ScaleToAsync(1.15, 800);
+                await Logo.ScaleToAsync(1.0, 800);
+            }
+        }   
     }
 }
