2234a51b9f397f2ad98db6d7c6a66e03d90a890b
Krystian <krystian.dziedzic.dev@protonmail.com>
2026-09-22 14:16:21 -0700

    Final concept of the UI

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,15 +2,13 @@
 {
     public partial class MainPage : ContentPage
     {
-        private bool isPopupVisible = false;
         public MainPage()
         {
             InitializeComponent();
         }
-
         private async void Animate_Logo(object sender, EventArgs e)
         {
-            while(true)
+            while (true)
             {
                 await Logo.ScaleToAsync(1.15, 800);
                 await Logo.ScaleToAsync(1.0, 800);
@@ -18,13 +16,13 @@
         }
         private void DisplayLoginPopup(object sender, EventArgs e)
         {
-            if (!isPopupVisible)
-            {
-                isPopupVisible = true;
-                LoginPopup.IsVisible = true;
-            }
-            //Chce zrobic animacje "wysuwania" popupu i takiego jakby przemieszczenia się loga na ten popup
-            //Ale nie mam juz dzisiaj czasu więc to dla jutrzejszego mnie ;)
+            LoginPopup.IsVisible = true;
+        }
+
+        private void LoginButtonClicked(object sender, EventArgs e)
+        {
+            // Logika logowania użytkownika (do bazy danych)
+            LoginPopup.IsVisible = false;
         }
     }
 }
