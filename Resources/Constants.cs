d13f8eef599df054440ae8c7bed810c3c2990a80
23.dziedzic.k <23.dziedzic.k@S50-10>
2026-09-23 15:07:32 +0200

    Database init

---
+++ Resources/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mUczen.Resources
+{
+    internal class Constants
+    {      
+
+        public const string DatabaseFilename = "mUczen.db3";
+
+        public const SQLite.SQLiteOpenFlags Flags =
+        SQLite.SQLiteOpenFlags.ReadWrite |
+        SQLite.SQLiteOpenFlags.Create |
+        SQLite.SQLiteOpenFlags.SharedCache;
+
+        public static string DatabasePath => Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename);
+    }
+}
