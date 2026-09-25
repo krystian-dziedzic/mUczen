65a44b6518f113495abe7fa252b84607424ad0de
Krystian <krystian.dziedzic.dev@protonmail.com>
2026-09-25 14:20:10 +0200

    Adding new models for database functionality

---
+++ Models/Attendance.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mUczen.Models
+{
+    internal class Attendance
+    {
+        List<Tuple<DateTime, bool>>? AttendanceList;
+        //DateTime - Data lekcji, bool - Obecność (true) lub nieobecność (false)
+    }
+}
---
+++ Models/Grades.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mUczen.Models
+{
+    internal class Grades
+    {
+        public List<Tuple<string, double>>? GradesList;
+        // String - Nazwa oceny (np. 6+) , double - Wartość oceny (np. 6.0)
+    }
+}
